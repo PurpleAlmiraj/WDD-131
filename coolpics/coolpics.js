319dd447da9804e05b6d2b76ba1ffc27d618fbe7
Ryan Jones <ryanbluej@gmail.com>
2024-10-10 09:35:29 -0700

    Bigger img

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -7,3 +7,48 @@ document.addEventListener('DOMContentLoaded', function () {
         nav.classList.toggle('active');
     });
 });
+
+function viewerTemplate(pic, alt) {
+    return `
+    <div class="viewer">
+        <button class="close-viewer">X</button>
+        <img src="${pic}" alt="${alt}">
+    </div>`;
+}
+
+function viewHandler(event) {
+
+    const clickedElement = event.target;
+
+
+    if (clickedElement.tagName === 'IMG') {
+
+        const imageSrc = clickedElement.getAttribute('src');
+
+        const imageParts = imageSrc.split('-');
+
+        const fullImageSrc = imageParts[0] + '-full.jpeg';
+
+        const altText = clickedElement.getAttribute('alt');
+
+        document.body.insertAdjacentHTML("afterbegin", viewerTemplate(fullImageSrc, altText));
+
+        const viewerElement = document.querySelector('.viewer');
+        viewerElement.style.visibility = 'visible';  
+
+        const closeButton = document.querySelector('.close-viewer');
+        closeButton.addEventListener('click', closeViewer);
+    }
+}
+
+function closeViewer() {
+    const viewerElement = document.querySelector('.viewer');
+    if (viewerElement) {
+        viewerElement.remove(); 
+    }
+}
+
+const gallery = document.querySelector('.gallery');
+if (gallery) {
+    gallery.addEventListener('click', viewHandler);
+}
